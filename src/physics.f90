3c02c72049bf097e2d1d6cd4d73688562e203402
Vanderlei Vargas Jr <vanderleirvjr@gmail.com>
2022-12-22 01:13:31 -0700

    Improved performance by 10%
    - Implemented update_bc subroutine

--- src/physics.f90
+++ src/physics.f90
@@ -18,96 +18,128 @@ module physics
         type(grid2d), intent(inout)                    :: grid
         real(kind=r_kind), intent(in)                  :: dt
         real(kind=r_kind), dimension(:,:), allocatable :: uwnd, vwnd, height
-        integer                                        :: i, j, yr, yl, xr, xl
+        real(kind=r_kind), dimension(:,:), allocatable :: tuwnd, tvwnd, theight
+        integer                                        :: i, j
         real(kind=r_kind)                              :: term1, term2, term3, term4
 
-        allocate(uwnd(grid%nx,grid%ny))
-        allocate(vwnd(grid%nx,grid%ny))
-        allocate(height(grid%nx,grid%ny))
+        allocate(uwnd(grid%nx + 2 ,grid%ny + 2 ))
+        allocate(vwnd(grid%nx + 2 ,grid%ny + 2 ))
+        allocate(height(grid%nx + 2 ,grid%ny + 2 ))
+        allocate(tuwnd(grid%nx + 2 ,grid%ny + 2 ))
+        allocate(tvwnd(grid%nx + 2 ,grid%ny + 2 ))
+        allocate(theight(grid%nx + 2 ,grid%ny + 2 ))
 
-        do i = 1, grid%nx 
-            do j = 1, grid%ny
-                xl = i - 1
-                xr = i + 1
-                yl = j - 1
-                yr = j + 1
-                if ( xl == 0 ) then
-                    xl = grid%nx
-                end if
-                if ( xr > grid%nx ) then
-                    xr = 1
-                end if
-                if ( yl == 0 ) then
-                    yl = grid%ny
-                end if
-                if ( yr > grid%ny ) then
-                    yr = 1
-                end if
-                term1 = grid%uwnd(i,j) * ( grid%uwnd(xr,j) - 2*grid%uwnd(i,j) + grid%uwnd(xl,j) ) / grid%dx * grid%dx
-                term2 = grid%vwnd(i,j) * ( grid%uwnd(i,yr) - 2*grid%uwnd(i,j) + grid%uwnd(i,yl) ) / grid%dy * grid%dy
-                term3 = GRAV * ( grid%height(xr,j) - 2*grid%height(i,j) + grid%height(xl,j) ) / grid%dx * grid%dx
-                uwnd(i,j) = grid%uwnd(i,j) - dt * ( term1 + term2 + term3) 
+        call update_bc(uwnd,vwnd,height,grid)
+
+        theight = height
+        tuwnd = uwnd
+        tvwnd = vwnd
+
+        do i = 2, grid%nx + 1
+            do j = 2, grid%ny + 1
+                term1 = uwnd(i,j) * ( uwnd(i+1,j) - 2*uwnd(i,j) + uwnd(i-1,j) ) / grid%dx * grid%dx
+                term2 = vwnd(i,j) * ( uwnd(i,j+1) - 2*uwnd(i,j) + uwnd(i,j-1) ) / grid%dy * grid%dy
+                term3 = GRAV * ( height(i+1,j) - 2*height(i,j) + height(i-1,j) ) / grid%dx * grid%dx
+                tuwnd(i,j) = uwnd(i,j) - dt * ( term1 + term2 + term3) 
             end do
         end do
+
+        do i = 2, grid%nx + 1
+            do j = 2, grid%ny + 1
+                term1 = uwnd(i,j) * ( vwnd(i+1,j) - 2*vwnd(i,j) + vwnd(i-1,j) ) / grid%dx * grid%dx
+                term2 = vwnd(i,j) * ( vwnd(i,j+1) - 2*vwnd(i,j) + vwnd(i,j-1) ) / grid%dy * grid%dy
+                term3 = GRAV * ( height(i,j+1) - 2*height(i,j) + height(i,j-1) ) / grid%dy * grid%dy
+                tvwnd(i,j) = vwnd(i,j) - dt * ( term1 + term2 + term3) 
+            end do
+        end do
+
+        uwnd = tuwnd
+        vwnd = tvwnd
+
+        do i = 2, grid%nx + 1
+            do j = 2, grid%ny + 1
+                term1 = uwnd(i,j) * ( height(i+1,j) - 2*height(i,j) + height(i-1,j) ) / grid%dx * grid%dx 
+                term2 = (mheight + height(i,j) ) * ( uwnd(i+1,j) - 2*uwnd(i,j) + uwnd(i-1,j) ) / grid%dx * grid%dx 
+                term3 = vwnd(i,j) * ( height(i,j+1) - 2*height(i,j) + height(i,j-1) ) / grid%dy * grid%dy
+                term4 = (mheight + height(i,j) ) * ( vwnd(i,j+1) -2*vwnd(i,j) + vwnd(i,j-1) ) / grid%dy * grid%dy
+                theight(i,j) = height(i,j) - dt * ( term1 + term2 + term3 + term4 )
+            end do
+        end do
+
+        height = theight
 
         do i = 1, grid%nx
             do j = 1, grid%ny
-                xl = i - 1
-                xr = i + 1
-                yl = j - 1
-                yr = j + 1
-                if ( xl == 0 ) then
-                    xl = grid%nx
-                end if
-                if ( xr > grid%nx ) then
-                    xr = 1
-                end if
-                if ( yl == 0 ) then
-                    yl = grid%ny
-                end if
-                if ( yr > grid%ny ) then
-                    yr = 1
-                end if
-                term1 = grid%uwnd(i,j) * ( grid%vwnd(xr,j) - 2*grid%vwnd(i,j) + grid%vwnd(xl,j) ) / grid%dx * grid%dx
-                term2 = grid%vwnd(i,j) * ( grid%vwnd(i,yr) - 2*grid%vwnd(i,j) + grid%vwnd(i,yl) ) / grid%dy * grid%dy
-                term3 = GRAV * ( grid%height(i,yr) - 2*grid%height(i,j) + grid%height(i,yl) ) / grid%dy * grid%dy
-                vwnd(i,j) = grid%vwnd(i,j) - dt * ( term1 + term2 + term3) 
+                grid%height(i,j) = height(i+1,j+1)
+                grid%uwnd(i,j) = uwnd(i+1,j+1)
+                grid%vwnd(i,j) = vwnd(i+1,j+1) 
             end do
         end do
-
-        grid%uwnd = uwnd
-        grid%vwnd = vwnd
-
-        do i = 1, grid%nx
-            do j = 1, grid%ny
-                xl = i - 1
-                xr = i + 1
-                yl = j - 1
-                yr = j + 1
-                if ( xl == 0 ) then
-                    xl = grid%nx
-                end if
-                if ( xr > grid%nx ) then
-                    xr = 1
-                end if
-                if ( yl == 0 ) then
-                    yl = grid%ny
-                end if
-                if ( yr > grid%ny ) then
-                    yr = 1
-                end if
-                term1 = grid%uwnd(i,j) * ( grid%height(xr,j) - 2*grid%height(i,j) + grid%height(xl,j) ) / grid%dx * grid%dx 
-                term2 = (mheight + grid%height(i,j) ) * ( grid%uwnd(xr,j) - 2*grid%uwnd(i,j) + grid%uwnd(xl,j) ) / grid%dx * grid%dx 
-                term3 = grid%vwnd(i,j) * ( grid%height(i,yr) - 2*grid%height(i,j) + grid%height(i,yl) ) / grid%dy * grid%dy
-                term4 = (mheight + grid%height(i,j) ) * ( grid%vwnd(i,yr) -2*grid%vwnd(i,j) + grid%vwnd(i,yl) ) / grid%dy * grid%dy
-                height(i,j) = grid%height(i,j) - dt * ( term1 + term2 + term3 + term4 )
-            end do
-        end do
-
-        grid%height = height
         
         return
 
     end subroutine update_state
+
+    subroutine update_bc(uwnd,vwnd,height,grid)
+
+        implicit none
+
+        type(grid2d), intent(in) :: grid
+        real(kind=r_kind), dimension(:,:), intent(inout) :: uwnd
+        real(kind=r_kind), dimension(:,:), intent(inout) :: vwnd
+        real(kind=r_kind), dimension(:,:), intent(inout) :: height
+        integer :: i, j
+
+        height = -999.
+        height(2:grid%nx+1,1)         = grid%height(:,grid%ny)
+        height(2:grid%nx+1,grid%ny+2) = grid%height(:,1)
+        height(1,2:grid%ny+1)         = grid%height(grid%nx,:)
+        height(grid%nx+2,2:grid%ny+1) = grid%height(1,:)
+        height(1,1)                   = grid%height(grid%nx,grid%ny)
+        height(grid%nx+2,grid%ny+2)   = grid%height(1,1)
+        height(1,grid%ny+2)           = grid%height(grid%nx,1)
+        height(grid%nx+2,1)           = grid%height(1,grid%ny)
+
+        do i = 1, grid%nx
+            do j = 1, grid%ny
+                height(i+1,j+1) = grid%height(i,j)
+            end do 
+        end do
+
+        uwnd = -999.
+        uwnd(2:grid%nx+1,1)         = grid%uwnd(:,grid%ny)
+        uwnd(2:grid%nx+1,grid%ny+2) = grid%uwnd(:,1)
+        uwnd(1,2:grid%ny+1)         = grid%uwnd(grid%nx,:)
+        uwnd(grid%nx+2,2:grid%ny+1) = grid%uwnd(1,:)
+        uwnd(1,1)                   = grid%uwnd(grid%nx,grid%ny)
+        uwnd(grid%nx+2,grid%ny+2)   = grid%uwnd(1,1)
+        uwnd(1,grid%ny+2)           = grid%uwnd(grid%nx,1)
+        uwnd(grid%nx+2,1)           = grid%uwnd(1,grid%ny)
+
+        do i = 1, grid%nx
+            do j = 1, grid%ny
+                uwnd(i+1,j+1) = grid%uwnd(i,j)
+            end do 
+        end do
+
+        vwnd = -999.
+        vwnd(2:grid%nx+1,1)         = grid%vwnd(:,grid%ny)
+        vwnd(2:grid%nx+1,grid%ny+2) = grid%vwnd(:,1)
+        vwnd(1,2:grid%ny+1)         = grid%vwnd(grid%nx,:)
+        vwnd(grid%nx+2,2:grid%ny+1) = grid%vwnd(1,:)
+        vwnd(1,1)                   = grid%vwnd(grid%nx,grid%ny)
+        vwnd(grid%nx+2,grid%ny+2)   = grid%vwnd(1,1)
+        vwnd(1,grid%ny+2)           = grid%vwnd(grid%nx,1)
+        vwnd(grid%nx+2,1)           = grid%vwnd(1,grid%ny)
+
+        do i = 1, grid%nx
+            do j = 1, grid%ny
+                vwnd(i+1,j+1) = grid%vwnd(i,j)
+            end do 
+        end do
+
+        return
+
+    end subroutine update_bc
 
 end module physics 

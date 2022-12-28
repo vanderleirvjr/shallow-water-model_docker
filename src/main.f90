9f4e7fbf5947157077a89a4c2c004d559a68037b
Vanderlei Vargas Jr <vanderlei.vargas@colostate.edu>
2022-12-28 11:05:02 -0700

    Preprocessing options added along with mpi library

--- src/main.f90
+++ src/main.f90
@@ -3,11 +3,16 @@ program shallow_water
 ! This is the main program responsible to call the
 ! main subroutines.
 
+#ifdef MPI    
+    use model_core_mpi, only: initialize, run, finalize
+
+#else 
     use model_core, only: initialize, run, finalize
+#endif
 
     implicit none
 
-    call initialize
+    call initialize    
 
     call run
 

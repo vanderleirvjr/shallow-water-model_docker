f3665292c741a809ac71e09856a3f7c441b2c0ee
Vanderlei Vargas Jr <vanderlei.vargas@colostate.edu>
2022-12-29 15:40:20 -0700

    Simplified code; model_core_mpi => model_core

--- src/main.f90
+++ src/main.f90
@@ -3,12 +3,7 @@ program shallow_water
 ! This is the main program responsible to call the
 ! main subroutines.
 
-#ifdef MPI    
-    use model_core_mpi, only: initialize, run, finalize
-
-#else 
     use model_core, only: initialize, run, finalize
-#endif
 
     implicit none
 
